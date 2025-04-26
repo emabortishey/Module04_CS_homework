57ae90e4d4e652696855f4d2c4678e7cd40ca92a
fartb <fartbreather228@gmail.com>
2025-04-26 17:50:35 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using static System.Console;
+
+public class Money
+{
+    char _sign;
+    int _whole;
+    int _fract;
+
+    public Money(char sign, int whole, int fract)
+    {
+        _sign = sign;
+        _whole = whole;
+        _fract = fract;
+    }
+
+    public char Sign
+    {
+        get { return _sign; }
+        set { _sign = value; }
+    }
+
+    public int Whole
+    {
+        get { return _whole; }
+        set { _whole = value; }
+    }
+
+    public int Fractional
+    {
+        get { return _fract; }
+        set { _fract = value; }
+    }
+}
